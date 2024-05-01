371f16048d9b990d0716e7fbd256ec93cce1592a
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-05-01 23:28:46 +0000

    test
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/istio-multi-region-setup/ansible.tf
+++ terraform/istio-multi-region-setup/ansible.tf
@@ -45,7 +45,7 @@ resource "null_resource" "run_ansible" {
   provisioner "local-exec" {
     command     = <<-EOT
           ansible-galaxy collection install ${var.ansible_collection_url},${var.ansible_collection_tag}
-          # ansible-playbook istio.iac.istio_multi_region_deploy -i ${local_sensitive_file.ansible_inventory.filename}
+          ansible-playbook istio.iac.istio_multi_region_deploy -i ${local_sensitive_file.ansible_inventory.filename}
     EOT
     working_dir = path.module
   }
