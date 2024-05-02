93ed635250f2eb7e05888e9fb2c317d05885f8d7
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-05-02 00:59:31 +0000

    test
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/base-infra/output.tf
+++ terraform/base-infra/output.tf
@@ -21,11 +21,26 @@ output "cls2_vcn_id" {
   value = module.cls2_vcn.vcn_id
 }
 
-
-# output "cls1_subnet_id" {
-#   value = module.cls1_vcn.subnet_id
-# }
-
 output "cls1_private_sub_1_id" {
   value = module.cls1_vcn.subnet_all_attributes.private_sub1.id
+}
+
+output "cls1_private_sub_2_id" {
+  value = module.cls1_vcn.subnet_all_attributes.private_sub2.id
+}
+
+output "cls1_public_sub_1_id" {
+  value = module.cls1_vcn.subnet_all_attributes.public_sub1.id
+}
+
+output "cls2_private_sub_1_id" {
+  value = module.cls2_vcn.subnet_all_attributes.private_sub1.id
+}
+
+output "cls2_private_sub_2_id" {
+  value = module.cls2_vcn.subnet_all_attributes.private_sub2.id
+}
+
+output "cls2_public_sub_1_id" {
+  value = module.cls2_vcn.subnet_all_attributes.public_sub1.id
 }
