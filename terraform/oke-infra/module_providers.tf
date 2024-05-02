6f81fec6353bb29639171230fb73445d83cd3f13
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-05-02 01:50:38 +0000

    testing
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/oke-infra/module_providers.tf
+++ terraform/oke-infra/module_providers.tf
@@ -1,4 +1,13 @@
 
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">=5.34.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
 
 provider "oci" {
   alias  = "region_1"
