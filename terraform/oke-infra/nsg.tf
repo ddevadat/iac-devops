34b3708b10a5ea6510d6a3394c194007ce181148
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-05-02 21:03:22 +0000

    ingess
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/oke-infra/nsg.tf
+++ terraform/oke-infra/nsg.tf
@@ -84,3 +84,48 @@ resource "oci_core_network_security_group_security_rule" "cls2_ingress_workers" 
   source_type               = "CIDR_BLOCK"
   provider                  = oci.region_2
 }
+
+##
+
+
+resource "oci_core_network_security_group_security_rule" "cls1_egress_cp" {
+  network_security_group_id = module.oke_cluster1.control_plane_nsg_id
+  direction                 = "EGRESS"
+  protocol                  = "all"
+  destination               = var.cls2_vcn_cidr
+  description               = "Cross Cluster Communication"
+  destination_type          = "CIDR_BLOCK"
+  provider                  = oci.region_1
+}
+
+resource "oci_core_network_security_group_security_rule" "cls1_ingress_cp" {
+  network_security_group_id = module.oke_cluster1.control_plane_nsg_id
+  direction                 = "INGRESS"
+  description               = "Cross Cluster Communication"
+  protocol                  = "all"
+  source                    = var.cls2_vcn_cidr
+  source_type               = "CIDR_BLOCK"
+  provider                  = oci.region_1
+}
+
+###
+
+resource "oci_core_network_security_group_security_rule" "cls2_egress_cp" {
+  network_security_group_id = module.oke_cluster2.control_plane_nsg_id
+  direction                 = "EGRESS"
+  protocol                  = "all"
+  destination               = var.cls1_vcn_cidr
+  description               = "Cross Cluster Communication"
+  destination_type          = "CIDR_BLOCK"
+  provider                  = oci.region_2
+}
+
+resource "oci_core_network_security_group_security_rule" "cls2_ingress_cp" {
+  network_security_group_id = module.oke_cluster2.control_plane_nsg_id
+  direction                 = "INGRESS"
+  description               = "Cross Cluster Communication"
+  protocol                  = "all"
+  source                    = var.cls1_vcn_cidr
+  source_type               = "CIDR_BLOCK"
+  provider                  = oci.region_2
+}
