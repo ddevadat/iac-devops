c7fce4a6a089a3f0a4966e71b422c46328a4a9ac
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-05-02 01:01:36 +0000

    test
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terraform/base-infra/output.tf
+++ terraform/base-infra/output.tf
@@ -21,6 +21,7 @@ output "cls2_vcn_id" {
   value = module.cls2_vcn.vcn_id
 }
 
+####
 output "cls1_private_sub_1_id" {
   value = module.cls1_vcn.subnet_all_attributes.private_sub1.id
 }
@@ -33,6 +34,7 @@ output "cls1_public_sub_1_id" {
   value = module.cls1_vcn.subnet_all_attributes.public_sub1.id
 }
 
+####
 output "cls2_private_sub_1_id" {
   value = module.cls2_vcn.subnet_all_attributes.private_sub1.id
 }
@@ -43,4 +45,32 @@ output "cls2_private_sub_2_id" {
 
 output "cls2_public_sub_1_id" {
   value = module.cls2_vcn.subnet_all_attributes.public_sub1.id
+}
+
+###
+
+output "cls1_private_sub_1_cidr_block" {
+  value = module.cls1_vcn.subnet_all_attributes.private_sub1.cidr_block
+}
+
+output "cls1_private_sub_2_cidr_block" {
+  value = module.cls1_vcn.subnet_all_attributes.private_sub2.cidr_block
+}
+
+output "cls1_public_sub_1_cidr_block" {
+  value = module.cls1_vcn.subnet_all_attributes.public_sub1.cidr_block
+}
+
+###
+
+output "cls2_private_sub_1_cidr_block" {
+  value = module.cls2_vcn.subnet_all_attributes.private_sub1.cidr_block
+}
+
+output "cls2_private_sub_2_cidr_block" {
+  value = module.cls2_vcn.subnet_all_attributes.private_sub2.cidr_block
+}
+
+output "cls2_public_sub_1_cidr_block" {
+  value = module.cls2_vcn.subnet_all_attributes.public_sub1.cidr_block
 }
