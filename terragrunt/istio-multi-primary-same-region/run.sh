1f09bb9e9b817cb41ae5d452a548de377f46cc6f
Deepak Devadathan <deepak.devadathan@oracle.com>
2024-05-03 00:45:29 +0000

    updated terragrunt
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- terragrunt/istio-multi-primary-same-region/run.sh
+++ terragrunt/istio-multi-primary-same-region/run.sh
@@ -7,3 +7,9 @@ terragrunt run-all init
 cd ../istio-multi-primary-same-region-oke-infra
 rm -rf .terragrunt-cache
 terragrunt apply -auto-approve --terragrunt-non-interactive
+
+
+terragrunt run-all init
+cd ../istio-multi-primary-same-region-setup
+rm -rf .terragrunt-cache
+terragrunt apply -auto-approve --terragrunt-non-interactive
